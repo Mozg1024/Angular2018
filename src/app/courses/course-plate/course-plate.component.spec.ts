28ce1ebb79b14c932e4cd95bd150a08f3bb2d628
Alexey Prokofiev <Aleksei_Prokofev@epam.com>
2019-01-20 21:48:46 +0400

    Course plate component covered by tests.

--- src/app/courses/course-plate/course-plate.component.spec.ts
+++ src/app/courses/course-plate/course-plate.component.spec.ts
@@ -2,10 +2,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursePlateComponent } from './course-plate.component';
 import { CourseModel } from '../course.model';
+import { By } from '@angular/platform-browser';
 
 describe('CoursePlateComponent', () => {
   let component: CoursePlateComponent;
   let fixture: ComponentFixture<CoursePlateComponent>;
+  let deleteButton;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,5 +30,27 @@ describe('CoursePlateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Delete button', () => {
+    beforeEach(() => {
+      deleteButton = fixture.debugElement.query(By.css('.delete-button'));
+    });
+
+    it('should call deleteCourse method', () => {
+      component.deleteCourse = jasmine.createSpy('deleteCourse');
+      deleteButton.triggerEventHandler('click', null);
+
+      expect(component.deleteCourse).toHaveBeenCalled();
+    });
+
+    it('should raise delete', () => {
+      let deletedCourseId;
+      component.delete.subscribe(courseId => deletedCourseId = courseId);
+
+      deleteButton.triggerEventHandler('click', null);
+
+      expect(deletedCourseId).toBe(component.course.id);
+    });
   });
 });
---
+++ src/app/courses/course-plate/course-plate.component.test-host.spec.ts
@@ -0,0 +1,56 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoursePlateComponent } from './course-plate.component';
+import { CourseModel } from '../course.model';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { Guid } from 'guid-typescript';
+
+@Component({
+  template: `
+    <app-course-plate
+      [course]="course"
+      (delete)="onCourseDelete($event)">
+    </app-course-plate>`
+})
+
+class TestHostComponent {
+  public course = new CourseModel({
+    title: 'title',
+    creationDate: new Date('01/01/2019'),
+    duration: 1000,
+    description: ['description']
+  });
+  public deletedCourseId: Guid;
+  public onCourseDelete(courseId: Guid) { this.deletedCourseId = courseId; }
+}
+
+describe('CoursePlateComponent into test host', () => {
+  let testHost: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CoursePlateComponent,
+        TestHostComponent,
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHost = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should raise delete', () => {
+    const deleteButton = fixture.debugElement.query(By.css('.delete-button'));
+
+    deleteButton.triggerEventHandler('click', null);
+
+    expect(testHost.deletedCourseId).toBe(testHost.course.id);
+  });
+});
